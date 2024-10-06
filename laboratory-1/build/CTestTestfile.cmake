22c3504b5cec902fc45549db6471bfd82006392d
Sergey Efimov <syefimov10@gmail.com>
2024-10-06 15:48:24 +0000

    laba2

--- laboratory-1/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/OOP-labs/laboratory-1
-# Build directory: /workspaces/OOP-labs/laboratory-1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MyProjectTests "/workspaces/OOP-labs/laboratory-1/build/tests")
-set_tests_properties(MyProjectTests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/OOP-labs/laboratory-1/CMakeLists.txt;34;add_test;/workspaces/OOP-labs/laboratory-1/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
--- laboratory-1/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/OOP-labs/laboratory-1/build/_deps/googletest-src/googletest
-# Build directory: /workspaces/OOP-labs/laboratory-1/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
